131391fafa7b478248ecb10582b68159e4b96725
Sellappullage Senuka Dishara Rosa <IT24104385@my.sliit.lk>
2025-05-16 15:50:53 +0530

    add ParkingFeeCalculator class with method stub

--- src/main/java/org/PG196VehicleParking/ParkingFeeCalculator.java
+++ src/main/java/org/PG196VehicleParking/ParkingFeeCalculator.java
@@ -1,4 +1,10 @@
 package org.PG196VehicleParking;
 
+import java.util.Date;
+
 public class ParkingFeeCalculator {
+    public static double calculateFee(Date entryTime, Date exitTime, String vehicleType, int doors, boolean hasSidecar) {
+        return 0.0;
+    }
 }
+
