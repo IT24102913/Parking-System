0c0a65031758aa17c9abbbf073ac01587a73ca16
IT24102678 <it24102678@my.sliit.lk>
2025-05-16 14:38:25 +0530

    class update

--- src/main/java/org/PG196VehicleParking/Car.java
+++ src/main/java/org/PG196VehicleParking/Car.java
@@ -3,10 +3,6 @@ package org.PG196VehicleParking;
 public class Car extends Vehicle {
     private int doors;
 
-    public Car(String plateNumber, int doors, String model) {
-        super(plateNumber,model);
-        this.doors = doors;
-    }
     public Car(String plateNumber, int doors) {
         super(plateNumber);
         this.doors = doors;
--- src/main/java/org/PG196VehicleParking/Motorbike.java
+++ src/main/java/org/PG196VehicleParking/Motorbike.java
@@ -1,14 +1,8 @@
-
-// Motorbike.java
 package org.PG196VehicleParking;
 
 public class Motorbike extends Vehicle {
     private boolean hasSidecar;
 
-    public Motorbike(String plateNumber, boolean hasSidecar, String model) {
-        super(plateNumber, model);
-        this.hasSidecar = hasSidecar;
-    }
     public Motorbike(String plateNumber, boolean hasSidecar) {
         super(plateNumber);
         this.hasSidecar = hasSidecar;
