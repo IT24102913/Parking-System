7129d2d90fa7be0fb7205251b945cd7667721a9c
Sellappullage Senuka Dishara Rosa <IT24104385@my.sliit.lk>
2025-05-17 05:49:59 +0530

    add flat hourly rate logic

--- src/main/java/org/PG196VehicleParking/ParkingFeeCalculator.java
+++ src/main/java/org/PG196VehicleParking/ParkingFeeCalculator.java
@@ -4,7 +4,9 @@ import java.util.Date;
 
 public class ParkingFeeCalculator {
     public static double calculateFee(Date entryTime, Date exitTime, String vehicleType, int doors, boolean hasSidecar) {
-        return 0.0;
+        long durationMillis = exitTime.getTime() - entryTime.getTime();
+        double hours = durationMillis / (1000.0 * 60 * 60);
+        return Math.ceil(hours) * 50.0;
     }
 }
 
