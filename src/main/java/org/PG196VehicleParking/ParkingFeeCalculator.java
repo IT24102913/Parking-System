25c34b2ede2841e743c05457ee65a7de7cd8c178
Sellappullage Senuka Dishara Rosa <IT24104385@my.sliit.lk>
2025-05-17 15:16:05 +0530

    implement type-based rates (Car vs Motorbike)

--- src/main/java/org/PG196VehicleParking/ParkingFeeCalculator.java
+++ src/main/java/org/PG196VehicleParking/ParkingFeeCalculator.java
@@ -8,5 +8,16 @@ public class ParkingFeeCalculator {
         double hours = durationMillis / (1000.0 * 60 * 60);
         return Math.ceil(hours) * 50.0;
     }
+
+    double rate;
+    if ("Car".equalsIgnoreCase(vehicleType)) {
+        rate = 100.0;
+    } else if ("Motorbike".equalsIgnoreCase(vehicleType)) {
+        rate = 50.0;
+    } else {
+        rate = 40.0;
+    }
+    return Math.ceil(hours) * rate;
+
 }
 
